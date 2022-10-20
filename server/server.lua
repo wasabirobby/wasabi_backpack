13f2a80b3fcaaef152e34e4786aebcbc2f87be63
wasabirobby <79250053+wasabirobby@users.noreply.github.com>
2022-10-19 19:42:38 -0700

    fix: duped identifiers, removal of shop requirement

--- server/server.lua
+++ server/server.lua
@@ -3,7 +3,7 @@
 ---------------------------------------------------------------
 local registeredStashes = {}
 
-function GenerateText(num) -- Thnx Linden
+local function GenerateText(num) -- Thnx Linden
 	local str
 	repeat str = {}
 		for i = 1, num do str[i] = string.char(math.random(65, 90)) end
@@ -12,21 +12,25 @@ function GenerateText(num) -- Thnx Linden
 	return str
 end
 
-exports('GenerateText', GenerateText)
-
-function GenerateSerial(text) -- Thnx Again
+local function GenerateSerial(text) -- Thnx Again
 	if text and text:len() > 3 then
 		return text
 	end
 	return ('%s%s%s'):format(math.random(100000,999999), text == nil and GenerateText(3) or text, math.random(100000,999999))
 end
 
-exports('GenerateSerial', GenerateSerial)
-
 RegisterServerEvent('wasabi_backpack:openBackpack')
 AddEventHandler('wasabi_backpack:openBackpack', function(identifier)
-    if not registeredStashes[identifier] then
+	if not registeredStashes[identifier] then
         exports.ox_inventory:RegisterStash('bag_'..identifier, 'Backpack', Config.BackpackStorage.slots, Config.BackpackStorage.weight, false)
         registeredStashes[identifier] = true
     end
+end)
+
+lib.callback.register('wasabi_backpack:getNewIdentifier', function(source, slot)
+	local newId = GenerateSerial()
+	exports.ox_inventory:SetMetadata(source, slot, {identifier = newId})
+	exports.ox_inventory:RegisterStash('bag_'..newId, 'Backpack', Config.BackpackStorage.slots, Config.BackpackStorage.weight, false)
+	registeredStashes[newId] = true
+	return newId
 end)
